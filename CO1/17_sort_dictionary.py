8f1649feb80a4be586db909e7b2eab71c4cc1292
Sandra S S <sandrasureshkumar123@gmail.com>
2022-03-08 04:21:48 +0530

    lab cycle

---
+++ CO1/17_sort_dictionary.py
@@ -0,0 +1,10 @@
+# Sort dictionary in ascending and descending order
+
+student = {
+    "name": "sandy",
+    "age": 22,
+    "place": "peyad"
+}
+
+print("Ascending order:", sorted(student.items()))
+print("Descending order:", sorted(student.items(), reverse=True))
